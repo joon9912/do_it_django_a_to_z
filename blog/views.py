bc337553c2cdb222751123ff70f956c32809aafc
joon9912 <joon9912@naver.com>
2022-03-21 18:33:43 +0900

    CBV로 블로그 상세 포스트 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostList(ListView):
@@ -19,13 +18,19 @@ def index(request):
     )
 '''
 
+class PostDetail(DetailView):
+    model = Post
+
+
+'''
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
 
     return render(
         request,
-        'blog/single_post_page.html',
+        'blog/post_detail.html',
         {
             'post': post,
         }
     )
+'''
